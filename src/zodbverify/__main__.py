efcc7d818b0d3f038811b45cf773af3366fe3206
Jens W. Klein <jk@kleinundpartner.at>
2019-05-09 12:22:49 +0200

    black

--- src/zodbverify/__main__.py
+++ src/zodbverify/__main__.py
@@ -9,16 +9,24 @@ from .verify import verify_zodb
 
 def main(argv=sys.argv):
     parser = argparse.ArgumentParser(
-        prog='zodbverify',
-        description='Verifies that all records in the database can be loaded.',
+        prog="zodbverify",
+        description="Verifies that all records in the database can be loaded.",
     )
     parser.add_argument(
-        '-f', '--zodbfile', action='store', dest='zodbfile', required=True,
-        help='Path to file-storage'
+        "-f",
+        "--zodbfile",
+        action="store",
+        dest="zodbfile",
+        required=True,
+        help="Path to file-storage",
     )
     parser.add_argument(
-        '-D', '--debug', action='store_true', dest='debug',
-        help='pause to debug broken pickles')
+        "-D",
+        "--debug",
+        action="store_true",
+        dest="debug",
+        help="pause to debug broken pickles",
+    )
     options = parser.parse_args(argv[1:])
 
     logging.basicConfig(level=logging.INFO)
@@ -26,5 +34,5 @@ def main(argv=sys.argv):
     verify_zodb(storage, debug=options.debug)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
